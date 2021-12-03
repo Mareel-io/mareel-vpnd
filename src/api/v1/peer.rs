a10428be4eccd81ebecad154ee13c5430f1bbd3f
Yonghyu Ban <yonghyu@empo.im>
2021-12-03 17:05:11 +0900

    Add state retrieval to peer API endpt

--- src/api/v1/peer.rs
+++ src/api/v1/peer.rs
@@ -1,14 +1,17 @@
-use rocket::serde::json::Json;
+use rocket::{serde::json::Json, State};
 
-use crate::api::common::ApiResponse;
+use crate::api::{common::ApiResponse, v1::InterfaceStore};
 
 use super::PeerConfig;
 
 #[post("/interface/<if_id>/peer", format = "json", data = "<peercfg>")]
 pub(crate) async fn create_peer(
+    iface_store: &State<InterfaceStore>,
     if_id: String,
     peercfg: Json<PeerConfig>,
 ) -> Option<Json<ApiResponse<String>>> {
+    //
+    //
     let ret: ApiResponse<String> = ApiResponse {
         status: Some("ok".to_string()),
         data: None,
@@ -17,17 +20,25 @@ pub(crate) async fn create_peer(
 }
 
 #[get("/interface/<if_id>/peer")]
-pub(crate) async fn get_peers(if_id: String) -> Option<Json<Vec<PeerConfig>>> {
+pub(crate) async fn get_peers(
+    iface_store: &State<InterfaceStore>,
+    if_id: String,
+) -> Option<Json<Vec<PeerConfig>>> {
     Some(Json(vec![]))
 }
 
 #[get("/interface/<if_id>/peer/<pubk>")]
-pub(crate) async fn get_peer(if_id: String, pubk: String) -> Option<Json<PeerConfig>> {
+pub(crate) async fn get_peer(
+    iface_store: &State<InterfaceStore>,
+    if_id: String,
+    pubk: String,
+) -> Option<Json<PeerConfig>> {
     None
 }
 
 #[put("/interface/<if_id>/peer/<pubk>", format = "json", data = "<peercfg>")]
 pub(crate) async fn update_peer(
+    iface_store: &State<InterfaceStore>,
     if_id: String,
     pubk: String,
     peercfg: Json<PeerConfig>,
@@ -36,6 +47,10 @@ pub(crate) async fn update_peer(
 }
 
 #[delete("/interface/<if_id>/peer/<pubk>")]
-pub(crate) async fn delete_peer(if_id: String, pubk: String) -> Option<String> {
+pub(crate) async fn delete_peer(
+    iface_store: &State<InterfaceStore>,
+    if_id: String,
+    pubk: String,
+) -> Option<String> {
     None
 }
