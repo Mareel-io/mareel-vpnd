3023812b83f319a1b80d735751bd819880e00a6f
Yonghyu Ban <yonghyu@empo.im>
2021-12-09 17:28:01 +0900

    Implement windows service management

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::net::IpAddr;
 use std::str::FromStr;
 
+use clap::Parser;
+
 use rocket::config::Config;
 use rocket::fairing::AdHoc;
 use rocket::tokio::sync::mpsc::Receiver;
@@ -46,12 +48,75 @@ pub(crate) async fn launch(shdn: Option<Receiver<()>>) -> Result<(), rocket::Err
         .await
 }
 
+#[derive(clap::Parser)]
+#[clap(about, version, author)]
+struct Args {
+    #[clap(long, short = 'i', value_name = "target")]
+    install: Option<String>,
+
+    #[clap(long, short = 'u', value_name = "target")]
+    uninstall: Option<String>,
+
+    #[clap(long, value_name = "target")]
+    start: Option<String>,
+
+    #[clap(long, value_name = "target")]
+    stop: Option<String>,
+
+    #[clap(short = 'c', value_name = "CONFIG")]
+    cfg: Option<String>,
+}
+
+fn main() -> Result<(), ()> {
+    // Do some magic
+    let args = Args::parse();
+
+    match (args.install, args.uninstall, args.start, args.stop) {
+        (None, None, None, None) => platform_main(),
+        (Some(_method), None, None, None) => {
+            #[cfg(not(target_os = "windows"))]
+            panic!("Not supported yet!");
+            #[cfg(target_os = "windows")]
+            winsvc::install().unwrap();
+            Ok(())
+        },
+        (None, Some(_method), None, None) => {
+            #[cfg(not(target_os = "windows"))]
+            panic!("Not supported yet!");
+            #[cfg(target_os = "windows")]
+            winsvc::uninstall().unwrap();
+            Ok(())
+        },
+        (None, None, Some(_method, ), None) => {
+            #[cfg(not(target_os = "windows"))]
+            panic!("Not supported yet!");
+            #[cfg(target_os = "windows")]
+            winsvc::start().unwrap();
+            Ok(())
+        },
+        (None, None, None, Some(_method, )) => {
+            #[cfg(not(target_os = "windows"))]
+            panic!("Not supported yet!");
+            #[cfg(target_os = "windows")]
+            winsvc::stop().unwrap();
+            Ok(())
+        },
+        (_, _, _, _) => panic!("Cannot do those things at the same time!"),
+    }
+}
+
 #[cfg(not(target_os = "windows"))]
-fn main() {
-    Runtime::new().unwrap().block_on(launch(None)).unwrap();
+fn platform_main() -> Result<(), ()> {
+    match Runtime::new().unwrap().block_on(launch(None)) {
+        Ok(_) => Ok(()),
+        Err(_) => Err(()), // TODO: Do it properly
+    }
 }
 
 #[cfg(target_os = "windows")]
-fn main() -> windows_service::Result<()> {
-    winsvc::run()
+fn platform_main() -> Result<(), ()> {
+    match winsvc::run() {
+        Ok(_) => Ok(()),
+        Err(_) => Err(()), // TODO: Do it properly
+    }
 }
--- src/winsvc.rs
+++ src/winsvc.rs
@@ -1,27 +1,31 @@
+// Most of code come from windows_service crate example
+// Licensed under Apache-2.0
+
 use rocket::futures::executor::block_on;
 use rocket::tokio::runtime::Runtime;
-use std::ffi::OsString;
+use std::ffi::{OsStr, OsString};
+use std::thread;
+use windows_service::service_control_handler::{self, ServiceControlHandlerResult};
+use windows_service::service_manager::{ServiceManager, ServiceManagerAccess};
 
 use std::time::Duration;
-use windows_service::{
-    define_windows_service,
-    service::{
-        ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus,
-        ServiceType,
-    },
-    service_control_handler::{self, ServiceControlHandlerResult},
-    service_dispatcher, Result,
+use windows_service::service::{
+    ServiceAccess, ServiceControl, ServiceControlAccept, ServiceErrorControl, ServiceExitCode,
+    ServiceInfo, ServiceStartType, ServiceState, ServiceStatus, ServiceType,
 };
+use windows_service::{define_windows_service, service_dispatcher};
 
 use rocket::tokio::sync::mpsc;
 
 #[cfg(target_os = "windows")]
 extern crate windows_service;
 
+const SERVICE_ID: &str = "mareel_vpn";
 const SERVICE_NAME: &str = "Mareel VPN";
 const SERVICE_TYPE: ServiceType = ServiceType::OWN_PROCESS;
+const SERVICE_DESC: &str = "Mareel VPN daemon";
 
-pub fn run() -> Result<()> {
+pub fn run() -> windows_service::Result<()> {
     service_dispatcher::start(SERVICE_NAME, ffi_service_main)
 }
 
@@ -33,7 +37,7 @@ pub fn service_main(_args: Vec<OsString>) {
     }
 }
 
-pub fn run_service() -> Result<()> {
+pub fn run_service() -> windows_service::Result<()> {
     let (shdn_tx, shdn_rx) = mpsc::channel::<()>(5);
 
     // Define system service event handler that will be receiving service events.
@@ -84,5 +88,65 @@ pub fn run_service() -> Result<()> {
         process_id: None,
     })?;
 
+    Ok(())
+}
+
+pub fn install() -> windows_service::Result<()> {
+    let manager_access = ServiceManagerAccess::CONNECT | ServiceManagerAccess::CREATE_SERVICE;
+    let service_manager = ServiceManager::local_computer(None::<&str>, manager_access)?;
+
+    // This example installs the service defined in `examples/ping_service.rs`.
+    // In the real world code you would set the executable path to point to your own binary
+    // that implements windows service.
+    let service_binary_path = ::std::env::current_exe().unwrap();
+
+    let service_info = ServiceInfo {
+        name: OsString::from(SERVICE_ID),
+        display_name: OsString::from(SERVICE_NAME),
+        service_type: ServiceType::OWN_PROCESS,
+        start_type: ServiceStartType::AutoStart,
+        error_control: ServiceErrorControl::Normal,
+        executable_path: service_binary_path,
+        launch_arguments: vec![],
+        dependencies: vec![],
+        account_name: None, // run as System
+        account_password: None,
+    };
+    let service = service_manager.create_service(&service_info, ServiceAccess::CHANGE_CONFIG)?;
+    service.set_description(SERVICE_DESC)?;
+    Ok(())
+}
+
+pub fn start() -> windows_service::Result<()> {
+    let manager_access = ServiceManagerAccess::CONNECT;
+    let service_manager = ServiceManager::local_computer(None::<&str>, manager_access)?;
+    let service = service_manager.open_service(SERVICE_ID, ServiceAccess::START)?;
+    service.start(&[OsStr::new("Started using CLI")])?;
+    Ok(())
+}
+
+pub fn stop() -> windows_service::Result<()> {
+    let manager_access = ServiceManagerAccess::CONNECT;
+    let service_manager = ServiceManager::local_computer(None::<&str>, manager_access)?;
+    let service = service_manager.open_service(SERVICE_ID, ServiceAccess::STOP)?;
+    service.stop()?;
+    Ok(())
+}
+
+pub fn uninstall() -> windows_service::Result<()> {
+    let manager_access = ServiceManagerAccess::CONNECT;
+    let service_manager = ServiceManager::local_computer(None::<&str>, manager_access)?;
+
+    let service_access = ServiceAccess::QUERY_STATUS | ServiceAccess::STOP | ServiceAccess::DELETE;
+    let service = service_manager.open_service(SERVICE_ID, service_access)?;
+
+    let service_status = service.query_status()?;
+    if service_status.current_state != ServiceState::Stopped {
+        service.stop()?;
+        // Wait for service to stop
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    service.delete()?;
     Ok(())
 }
