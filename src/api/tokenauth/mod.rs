478aa0ae0bfc475dbded7fbc79da33e3bc47f4a9
Yonghyu Ban <yonghyu@empo.im>
2022-01-24 02:16:27 +0900

    Remove keytar code

--- src/api/tokenauth/mod.rs
+++ src/api/tokenauth/mod.rs
@@ -3,32 +3,6 @@ use rocket::request::{FromRequest, Outcome, Request};
 
 use super::AuthKeyProvider;
 
-#[cfg(feature = "keytar")]
-use std::sync::Arc;
-
-#[cfg(feature = "keytar")]
-const KEYTAR_PACKAGE_NAME: &str = "io.mareel.vpn.vpnd";
-#[cfg(feature = "keytar")]
-const KEYTAR_ACC_NAME: &str = "apikey";
-
-#[cfg(feature = "keytar")]
-lazy_static! {
-    static ref APITOKEN: Arc<Option<String>> = {
-        // Load API key from keytar
-        let keyresult = match keytar::get_password(KEYTAR_PACKAGE_NAME, KEYTAR_ACC_NAME) {
-            Ok(x) => x,
-            Err(_) => return Arc::new(None),
-        };
-
-        // Ugly API but I cannot do anything about it for now...
-        // Track issue https://github.com/stoically/keytar-rs/issues/2
-        Arc::new(match keyresult.success {
-            true => Some(keyresult.password),
-            false => None,
-        })
-    };
-}
-
 pub struct ApiKey;
 
 #[rocket::async_trait]
