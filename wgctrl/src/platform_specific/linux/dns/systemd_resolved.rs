c80847229160a4f4bcf8eccf8f1ca04ded1da79d
Yonghyu Ban <yonghyu@empo.im>
2022-01-28 20:14:56 +0900

    Remove DNS Domain config

--- wgctrl/src/platform_specific/linux/dns/systemd_resolved.rs
+++ wgctrl/src/platform_specific/linux/dns/systemd_resolved.rs
@@ -61,14 +61,6 @@ impl SystemdResolved {
             log::error!("Failed to disable DoT: {}", error.display_chain());
         }
 
-        if let Err(error) = self
-            .dbus_interface
-            .set_domains(tunnel_index, &[(".", true)])
-            .await
-        {
-            log::error!("Failed to set search domains: {}", error.display_chain());
-        }
-
         let _ = self
             .dbus_interface
             .set_dns(self.tunnel_index, servers.to_vec())
@@ -78,14 +70,6 @@ impl SystemdResolved {
     }
 
     pub async fn reset(&mut self) -> Result<()> {
-        if let Err(error) = self
-            .dbus_interface
-            .set_domains(self.tunnel_index, &[])
-            .await
-        {
-            log::error!("Failed to set search domains: {}", error.display_chain());
-        }
-
         let _ = self
             .dbus_interface
             .set_dns(self.tunnel_index, vec![])
