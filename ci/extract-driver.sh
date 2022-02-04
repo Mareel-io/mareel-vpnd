2d5a48234053d8c59703d1b621986d7ed8193eab
Yonghyu Ban <yonghyu@empo.im>
2022-02-04 19:12:15 +0900

    Include windns.dll in artifact

--- ci/extract-driver.sh
+++ ci/extract-driver.sh
@@ -5,3 +5,4 @@
 # SPDX-License-Identifier: CC0-1.0
 
 unzip -j wireguard-nt.zip wireguard-nt/bin/amd64/wireguard.dll -d bin/x86_64-pc-windows-msvc/release/
+unzip -j windll.zip windns/x64-Release/windns.dll -d bin/x86_64-pc-windows-msvc/release/
