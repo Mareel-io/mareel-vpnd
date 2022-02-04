0aeb0ad21f0db4bb96c927154a1ad0571928ce49
Yonghyu Ban <yonghyu@empo.im>
2022-02-04 17:03:53 +0900

    Attempt to build on Windows with DLL

---
+++ wgctrl/build.rs
@@ -0,0 +1,27 @@
+use std::{env, path::PathBuf};
+
+fn manifest_dir() -> PathBuf {
+    env::var("CARGO_MANIFEST_DIR")
+        .map(PathBuf::from)
+        .expect("CARGO_MANIFEST_DIR env var not set")
+}
+
+fn main() {
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
+
+    let windns = manifest_dir().join("../windns/x64-Release");
+
+    match (target_arch.as_str(), target_os.as_str(), target_env.as_str()) {
+        (_arch, "windows", "msvc") => {
+            println!("cargo:rustc-link-search={}", &windns.display());
+            println!("cargo:rustc-link-lib=static=windns");
+        },
+        (_arch, "windows", target_env) => {
+            panic!("Sorry, env {} is not supported on Windows", target_env);
+        },
+        (_, _, _) => {
+        },
+    }
+}
